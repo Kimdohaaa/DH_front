6bdd49b7802a9e10defe5c0139055b897d2faff1
Kimdohaaa <jjeenee0408@gmail.com>
2024-11-20 12:48:02 +0900

    Day_8

---
+++ Day_7/1_JS시작.js
@@ -0,0 +1,10 @@
+console.log('[2]안녕 JS')
+console.log('[3]console 함수 출력')
+alert('[4]alert 함수 출력')
+document.write('[5]document.write 함수 출력')
+document.body.innerHTML('[6]document.body.innerHTML 속성 출력')
+
+
+confirm('[7] 확인 메세지 창')
+prompt('[8] 내용 입력 메세지 창')
+document.querySelector('body').innerHTML
